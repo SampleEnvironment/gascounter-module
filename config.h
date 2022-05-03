846b5af213ec565141c09bee124e9d976a16a196
Wegmann Peter <peter.wegmann@helmholtz-berlin.de>
2022-05-03 17:11:09 +0200

    added config

---
+++ config.h
@@ -0,0 +1,24 @@
+/*
+ * config.h
+ *
+ * Created: 03.05.2022 11:31:56
+ *  Author: qfj
+ */ 
+
+
+#ifndef CONFIG_H_
+#define CONFIG_H_
+
+
+// Display Version:
+#define ili9341
+#define old_LCD 
+
+
+#define FIRMWARE_VERSION  209 /**< @brief  Software Version  */
+
+#define BRANCH_ID 2
+
+
+
+#endif /* CONFIG_H_ */
