f36be986984e5a7042a9c1f0c4993db5266a7825
Peter Wegmann <wegmannp@hu-berlin.de>
2022-09-28 21:32:12 +0200

    moved config to config.h file

--- config.h
+++ config.h
@@ -11,13 +11,18 @@
 
 
 // Display Version:
-//#define ili9341
-//#define old_LCD 
+#define ili9341
+//#define GCM_old_disp 
+
+// Network Hardware
+#define USE_XBEE
+//#define USE_LAN
 
 
 #define FIRMWARE_VERSION  212 /**< @brief  Software Version  */
 
 #define BRANCH_ID 2
+
 
 #define HZB
 
